a18bf82cc5934730c7ca3b1e7d0f4e9afab2cead
FedorVisa <floodywizard@gmail.com>
2020-11-16 18:51:37 +0300

    cdal labu

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -1,6 +1,5 @@
 ﻿#include <stdio.h>
 
-
 int mod_(int dat) {
     if (dat < 0) {
         return dat * -1;
@@ -8,11 +7,9 @@ int mod_(int dat) {
     return dat;
 }
 
-
-int minimum( int* d, int len)
+int findingMinimum( int* d, int len)
 {
     int min = d[0];
-  
     for (int i = 0; i < len; i++) {
         if (mod_(min)>mod_(d[i])) {
             min = d[i];
@@ -21,22 +18,26 @@ int minimum( int* d, int len)
     return min;
 }
 
-
-
 int main()
 {
-
     int d[10];
-    int n = 10;
-    int min = 1000000;
+    int n = 10; 
     int len = 0;
-    scanf("%d", &len);
-
+    int c;
+    int flag;
+    printf("enter numder of elements: ");
+    c=scanf("%d", &len);
+    if (c != 1) {
+        flag = 1;
+        return printf("not a number");
+    }
+    if (len<0)  return printf("ne polozitelnoe chislo");
+    printf("enter elements: ");
     for (int j = 0; j < len; j++) {
         scanf("%d", &j[d]);
     }
-    printf("min element: %d", minimum(d,len));
-        return 0;
+    printf("min element: %d", findingMinimum(d,len));
+    return 0;
 }
 
 
